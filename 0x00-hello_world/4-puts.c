334de9cd8d038df4848f17e9fd289f076a858376
root <rozenhertfatima>
2023-06-15 03:57:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,11 @@
  *
  * Return: Always 0 (Success)
  */
-     int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzl");
 
-	  return (0);
+
+       	return (0);
 
 } 
