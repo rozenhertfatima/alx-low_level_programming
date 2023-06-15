5b16e8bfbf97ade8d25cb7f5349f5334e662156b
root <rozenhertfatima>
2023-06-15 04:03:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,9 +9,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzl");
-
-
-       	return (0);
-
-} 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
