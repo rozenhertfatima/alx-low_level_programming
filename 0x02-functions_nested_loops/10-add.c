9bb5b1c048cdde88d1135b5b525fd3af8b1ec582
root <rozenhertfatima>
2023-06-20 13:20:29 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "main.h"
+/**
+ * add - adds to number.
+ *
+ *@first_num: first number
+ *@second_num: second number
+ *
+ *Return: to result.
+ */
+int add(int first_num, int second_num)
+{
+	int result;
+
+	result = first_num + second_num;
+	return (result);
+}
+
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - starting from 00:00 to 23:59.
+ *
+ *Return: NO return.
+ */
+
+	void jackBauer(void)
+	{
+		int h , m;
+
+		for (h = 0; h < 24; h++)
+		{
+			for (m  = 0; m  < 60; m++)
+			{
+				_putchar("m");
+
+				_putchar("h");
+			}
+		}
+	}
+	return (0);
