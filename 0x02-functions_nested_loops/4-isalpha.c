e0061414d7615626c3ad0d2111fd36a2ece18d13
root <rozenhertfatima>
2023-06-20 07:17:05 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isalpha - checks for alphabetic character
+ * @c: integer var
+ *
+ * Return: return 1 of c is lowercase, else return to 0.
+ */
+int _isalpha(int c)
+{
+    if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+        return (1);
+    else
+        return (0);
+}
