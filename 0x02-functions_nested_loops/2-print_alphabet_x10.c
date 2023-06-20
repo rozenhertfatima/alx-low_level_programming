cfae32be2941ad53df34a6be18e3b509c439c406
root <rozenhertfatima>
2023-06-20 04:59:06 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet, in lowercase.
+ *
+ * Return: NO return
+ */
+
+void print_alphabet_x10(void)
+{
+	char x = 'a' ,i;
+	for (i = 0; i < 10; i++)
+	{
+		{
+			for (x = 97; x <= 122; x++)
+
+				_putchar(x);
+		}
+		_putchar('\n');
+	}	}
