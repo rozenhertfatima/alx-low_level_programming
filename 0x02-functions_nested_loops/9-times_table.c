aab5a1381e705b666a29d3e308f2da7ee776ae11
root <rozenhertfatima>
2023-06-21 02:35:47 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,9 +17,9 @@ void times_table(void)
 			_putchar(' ');
 			if ((a * b) < 9)
 				_putchar(' ');
-				else
+			else
 				_putchar((a * b) / 10 + 48);
-				_putchar((a * b) % 10 + 48);
+			_putchar((a * b) % 10 + 48);
 		}
 		_putchar('\n');
 	}
