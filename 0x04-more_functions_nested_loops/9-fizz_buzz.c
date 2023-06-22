94e44e8e60e725c574b659a75b3dcade341b65a3
root <rozenhertfatima>
2023-06-22 10:10:51 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,19 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (n % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (n % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", n);
-	}
-	printf("\n");
+			printf("%d", n);
 
-	return (0);
+		if (n != 100)
+			printf(" ");
+	}
+
+	printf("\n");
+	return 0;
 }
+
